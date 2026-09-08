5e0aee87d3b698f0e47eb6cf8991becfa13a9ae5
JuanRodriguez <juanrodriguez1117659@correo.itm.edu.co>
2026-09-07 22:58:17 -0500

    Se añade la clase Arbitro

---
+++ src/model/domain/Arbitro.java
@@ -0,0 +1,29 @@
+package src.model.domain;
+
+public class Arbitro extends Persona {
+    private String categoria;
+
+    public Arbitro(String identificacion, String nombre, String categoria) {
+        super(identificacion, nombre);
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String rolEnPartido() {
+        return "Árbitro [Categoría: " + categoria + "]";
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String datosResumen() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'datosResumen'");
+    }
+}
